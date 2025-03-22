f3a606bcc7bf9b3ad21e3427f43811c19437fa87
Lucas Gomes Santana <ls7128387@gmail.com>
2025-03-22 11:43:42 -0300

    Final Adjusts on the Code Challenge

--- src/main/java/banco/itau/desafio/controller/EstatisticasController.java
+++ src/main/java/banco/itau/desafio/controller/EstatisticasController.java
@@ -2,6 +2,7 @@ package banco.itau.desafio.controller;
 
 import banco.itau.desafio.dto.EstatisticasDTO;
 import banco.itau.desafio.service.TransacaoService;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/banco/itau/desafio/dto/EstatisticasDTO.java
+++ src/main/java/banco/itau/desafio/dto/EstatisticasDTO.java
@@ -1,5 +1,7 @@
 package banco.itau.desafio.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+
 import java.util.DoubleSummaryStatistics;
 
 
@@ -15,8 +17,16 @@ public class EstatisticasDTO {
         this.count = statistics.getCount();
         this.sum = statistics.getSum();
         this.avg = statistics.getAverage();
-        this.min = statistics.getMin();
-        this.max = statistics.getMax();
+
+        // Para não retornar Infinity nos campos min e max
+        if(statistics.getCount() == 0){
+            this.min = 0;
+            this.max = 0;
+        }
+        else{
+            this.max = statistics.getMax();
+            this.min = statistics.getMin();
+        }
     }
 
     public long getCount() {
